2db43a8d28dffb8794b66aadc36f1166c3a6ebc3
Felix Segerer <don.felice@gmx.net>
2023-08-06 13:20:28 +0200

    feat: add depth option for tree and major refactor

--- clifs/plugins/tree.py
+++ clifs/plugins/tree.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from argparse import ArgumentParser
+from abc import ABC, abstractmethod
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
+from typing import List, Optional
 
 from clifs.clifs_plugin import ClifsPlugin
 from clifs.utils_cli import ANSI_COLORS, size2str, wrap_string
@@ -11,6 +13,176 @@ ELBOW = "└──"
 TEE = "├──"
 PIPE_PREFIX = "│   "
 SPACE_PREFIX = "    "
+SPACE_SIZE = "  "
+
+
+class Entry(ABC):
+    """
+    Base class for entries in a DirectoryTree
+    """
+
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        path: Path,
+        prefix: str = "",
+        connector: str = "",
+        depth: int = 0,
+        plot_size: bool = True,
+    ):
+        self.path = path
+        self.prefix = prefix
+        self.connector = connector
+        self.depth = depth
+        self.plot_size = plot_size
+
+        self.name: str = self.path.name
+        self.size: Optional[float] = None
+
+    @abstractmethod
+    def __str__(self) -> str:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_size(self) -> Optional[float]:
+        """Get the overall size of the entry"""
+        raise NotImplementedError
+
+
+class File(Entry):
+    """Representing files in a Diretory Tree"""
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.size: Optional[float] = self.get_size() if self.plot_size else None
+
+    def get_size(self) -> Optional[float]:
+        try:
+            return self.path.stat().st_size
+        except FileNotFoundError:
+            file_long = Path(
+                "\\\\?\\" + str(self.path)
+            )  # handle long paths in windows systems
+            return file_long.stat().st_size
+
+    def __str__(self) -> str:
+        string = f"{self.prefix}{self.connector} {self.name}"
+        if self.plot_size and self.size is not None:
+            string += SPACE_SIZE + size2str(self.size)
+        return string
+
+
+class Folder(Entry):
+    """
+    Reprensenting folders in a DirectoryTree.
+    """
+
+    def __init__(
+        self,
+        dirs_only: bool = False,
+        depth_th: Optional[int] = None,
+        folder_color: str = "yellow",
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+
+        self.dirs_only = dirs_only
+        self.depth_th = depth_th
+        self.folder_color = folder_color
+
+        self.have_access: bool = True
+        self.children: List[Entry] = []
+
+        if (self.depth_th is None or self.depth < self.depth_th) or self.plot_size:
+            self.children = self.get_children()
+
+        self.size: Optional[float] = self.get_size() if self.plot_size else None
+
+    def get_children(self) -> List[Entry]:
+        children: List[Entry] = []
+
+        items = list(self.path.iterdir())
+        try:
+            items = sorted(items, key=lambda item: (not item.is_file(), str(item)))
+            for num_item, item in enumerate(items):
+                child_connector = ELBOW if num_item == len(items) - 1 else TEE
+
+                child_prefix = self.prefix
+                if self.connector == TEE:  # not last dir
+                    child_prefix += PIPE_PREFIX
+                elif self.connector == ELBOW:  # last dir
+                    child_prefix += SPACE_PREFIX
+
+                if item.is_file():
+                    if not self.dirs_only or self.plot_size:
+                        children.append(
+                            File(
+                                path=item,
+                                prefix=child_prefix,
+                                connector=child_connector,
+                                depth=self.depth + 1,
+                                plot_size=self.plot_size,
+                            )
+                        )
+                if item.is_dir():
+                    children.append(
+                        Folder(
+                            path=item,
+                            prefix=child_prefix,
+                            connector=child_connector,
+                            depth=self.depth + 1,
+                            depth_th=self.depth_th,
+                            dirs_only=self.dirs_only,
+                            plot_size=self.plot_size,
+                        )
+                    )
+            return children
+
+        except PermissionError as err:
+            print(
+                wrap_string(
+                    f'Warning: no permission to access "{self.path}". '
+                    f"Size calculations of parent directories could be off.",
+                    prefix=ANSI_COLORS["red"],
+                )
+            )
+            print(wrap_string(f'Error message: "{err}"', prefix=ANSI_COLORS["red"]))
+            self.have_access = False
+            return []
+
+    def get_size(self) -> Optional[float]:
+        if not self.have_access:
+            return None
+        size = 0.0
+        for child in self.children:
+            size += child.size if child.size is not None else size
+        return size
+
+    def __str__(self) -> str:
+        if self.depth != 0:
+            string = (
+                f"{self.prefix}{self.connector} "
+                f"{wrap_string(self.name, prefix=ANSI_COLORS[self.folder_color])}"
+            )
+        else:
+            string = wrap_string(self.name, prefix=ANSI_COLORS[self.folder_color])
+
+        if not self.have_access:
+            string += SPACE_SIZE + wrap_string("no access", prefix=ANSI_COLORS["red"])
+        elif self.plot_size and self.size is not None:
+            string += SPACE_SIZE + size2str(self.size)
+
+        if (self.depth_th is None or self.depth < self.depth_th) and self.children:
+            if self.dirs_only:
+                dir_children = [
+                    child.__str__()
+                    for child in self.children
+                    if isinstance(child, Folder)
+                ]
+                if dir_children:
+                    string += "\n" + "\n".join(dir_children)
+            else:
+                string += "\n" + "\n".join([child.__str__() for child in self.children])
+        return string
 
 
 class DirectoryTree(ClifsPlugin):
@@ -21,9 +193,10 @@ class DirectoryTree(ClifsPlugin):
     root_dir: Path
     dirs_only: bool
     hide_sizes: bool
+    depth: int
 
     @staticmethod
-    def init_parser(parser: ArgumentParser):
+    def init_parser(parser: ArgumentParser) -> None:
         """
         Adding arguments to an argparse parser. Needed for all clifs_plugins.
         """
@@ -48,91 +221,25 @@ class DirectoryTree(ClifsPlugin):
             default=False,
             help="Hide size information",
         )
-
-    def __init__(self, args):
-        super().__init__(args)
-        self._tree = []
-
-    def run(self):
-        self._add_directory(self.root_dir)
-        for entry in self._tree:
-            print(entry)
-
-    def _add_directory(  # pylint: disable=too-many-arguments
-        self,
-        directory,
-        index=0,
-        entries_count=0,
-        prefix="",
-        connector="",
-        ansiescape_color=ANSI_COLORS["yellow"],
-    ) -> int:
-        idx_dir = len(
-            self._tree
-        )  # get index of current directory in tree list to attach size info
-        self._tree.append(
-            f"{prefix}{connector}"
-            + wrap_string(f"{directory.name}", prefix=ansiescape_color)
+        parser.add_argument(
+            "-d",
+            "--depth",
+            type=int,
+            default=None,
+            help="Max depth to which the tree is plotted",
         )
 
-        if connector == "":  # for root dir
-            pass
-        elif index != entries_count - 1:
-            prefix += PIPE_PREFIX
-        else:  # for last sub-dir
-            prefix += SPACE_PREFIX
+    def __init__(self, args: Namespace) -> None:
+        super().__init__(args)
+        self.dir: Folder = Folder(
+            path=self.root_dir.resolve(),
+            plot_size=not self.hide_sizes,
+            depth_th=self.depth,
+            dirs_only=self.dirs_only,
+        )
 
-        entries = directory.iterdir()
-        try:
-            entries = sorted(entries, key=lambda item: (not item.is_file(), str(item)))
-        except PermissionError as err:
-            print(
-                wrap_string(
-                    f'Warning: no permission to access "{directory}". '
-                    f"Size calculations of parent directories could be off.",
-                    prefix=ANSI_COLORS["red"],
-                )
-            )
-            print(wrap_string(f'Error message: "{err}"', prefix=ANSI_COLORS["red"]))
-            self._tree[idx_dir] = self._tree[idx_dir] + wrap_string(
-                SPACE_PREFIX + "no access", prefix=ANSI_COLORS["red"]
-            )
-            return 0
+    def __str__(self):
+        return self.dir.__str__()
 
-        entries_count = len(entries)
-        size = 0  # initialize size of sub-directories and files
-
-        for sub_index, entry in enumerate(entries):
-            connector = ELBOW if sub_index == entries_count - 1 else TEE
-            if entry.is_dir():
-                size += self._add_directory(
-                    entry, sub_index, entries_count, prefix, connector
-                )
-            else:
-                if self.dirs_only and self.hide_sizes:
-                    pass
-                else:
-                    size += self._add_file(entry, prefix, connector)
-
-        if not self.hide_sizes:
-            self._tree[idx_dir] = self._tree[idx_dir] + wrap_string(
-                "  " + size2str(size), prefix=ansiescape_color
-            )
-        return size
-
-    def _add_file(self, file, prefix, connector) -> int:
-        try:
-            size = file.stat().st_size if not self.hide_sizes else 0
-        except FileNotFoundError:
-            file_long = Path(
-                "\\\\?\\" + str(file)
-            )  # handle long paths in windows systems
-            size = file_long.stat().st_size if not self.hide_sizes else 0
-        if not self.dirs_only:
-            if not self.hide_sizes:
-                self._tree.append(
-                    f"{prefix}{connector} {file.name}" + "  " + size2str(size)
-                )
-            else:
-                self._tree.append(f"{prefix}{connector} {file.name}")
-        return size
+    def run(self):
+        print(self)
